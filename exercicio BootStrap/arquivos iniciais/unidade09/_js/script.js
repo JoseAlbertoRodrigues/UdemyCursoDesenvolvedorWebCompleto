774ac444ff09bfe245e3c8b31474c18321453cf0
José Alberto Rodrigues Neto <josealberto270920@gmail.com>
2023-09-23 18:33:51 -0300

    Section 49: Course V - Unit 09 - Creating an image carousel - 228. Adjusting the carousel's JS and CSS -- BootStrap

---
+++ exercicio BootStrap/arquivos iniciais/unidade09/_js/script.js
@@ -0,0 +1,7 @@
+$(function() {
+    $('.carousel').carousel({
+        interval: 3000,
+        pause: "null", // quando o mouse ficar em cima da foto, não ira ter efeito
+        wrap: true // para a foto continuar passando para a proxima
+    })
+})
