5e1dcd14027745aed52c11bdfac775c9dfb79029
José Alberto Rodrigues Neto <josealberto270920@gmail.com>
2024-09-26 01:01:22 -0300

    files added for PHP classes, but the classes were made with other repositories, because they need to be on the XAMPP server.

---
+++ exercicio PHP/fundamental/padrao/inicial.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso PHP FUNDAMENTAL</title>
+    </head>
+        <?php
+            echo "JOSÉ ALBERTO RODRIGUES NETO"
+        ?>
+    <body>
+    </body>
+</html>
